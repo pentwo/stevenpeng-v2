9f3824a6db00f5b71eb6f0c8bcefb53df2a887a3
Steven Peng <pentwo@gmail.com>
2023-04-11 14:43:38 +0800

    FEAT: Layout the page in the gatsby-browser.js

---
+++ gatsby-browser.js
@@ -0,0 +1,9 @@
+const React = require("react")
+const Layout = require("./src/components/layout")
+
+
+
+// Wraps every page in a component
+exports.wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
+}
